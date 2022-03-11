87e8616566c9116f9ef1b1fba54be45863747f96
yubin <yubin@itdadev.com>
2022-03-11 23:44:11 +0900

    WIP login

--- src/pages/Sign up/SignupForm.js
+++ src/pages/Sign up/SignupForm.js
@@ -1,16 +1,70 @@
 import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+
 import Logo from "../../assets/images/Logo.png";
+
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
 import "../Root.css";
 
 const SignupForm = ({ setSignUp }) => {
+  const [signupForm, setSignUpForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = signupForm;
+
+  const auth = getAuth();
+
+  const handleChange = (e) => {
+    e.preventDefault();
+
+    const { value, name } = e.target;
+
+    setSignUpForm({
+      ...signupForm,
+      [name]: value,
+    });
+  };
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+        toast.success("dsf");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+  };
+
   return (
     <>
+      <ToastContainer />
       <form className="container">
         <img src={Logo} alt="Logo" />
         <h1>Sign up</h1>
-        <input placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <button onClick={() => setSignUp()}>Sign up</button>
+        <input
+          onChange={handleChange}
+          name="email"
+          value={email}
+          placeholder="Email"
+        />
+        <input
+          onChange={handleChange}
+          name="password"
+          value={password}
+          type="password"
+          placeholder="Password"
+        />
+        <button onClick={handleSignUp}>Sign up</button>
 
         <button>Continue with Google</button>
         <button>Continue with KakaoTalk</button>
---
+++ src/serivces/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBZmT21iZn2twoc1kEpWjQrmHj638IvrnA",
+  authDomain: "ruby-74156.firebaseapp.com",
+  projectId: "ruby-74156",
+  storageBucket: "ruby-74156.appspot.com",
+  messagingSenderId: "333823804103",
+  appId: "1:333823804103:web:68b8239fafffaa431874e2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
